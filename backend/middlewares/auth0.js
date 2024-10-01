1108392573aa844ff25796911643b085b2345eb7
Keys4Ever <cubillagonzalo@outlook.com>
2024-10-01 01:22:37 -0300

    Fix: some little changes

--- backend/middlewares/auth0.js
+++ backend/middlewares/auth0.js
@@ -1,48 +1,45 @@
-import { auth } from 'express-openid-connect';
-import dotenv from 'dotenv';
-import client from '../config/turso.js';
+import { auth } from "express-openid-connect";
+import dotenv from "dotenv";
+import client from "../config/turso.js";
 
 dotenv.config();
 
 const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: process.env.AUTH0_SECRET,
-    baseURL: process.env.AUTH0_BASEURL,
-    clientID: process.env.AUTH0_CLIENTID,
-    issuerBaseURL: process.env.AUTH0_ISSUERBASE
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_SECRET,
+  baseURL: process.env.AUTH0_BASEURL,
+  clientID: process.env.AUTH0_CLIENTID,
+  issuerBaseURL: process.env.AUTH0_ISSUERBASE,
 };
 
 const authMiddleware = auth(config);
 
 const checkUserInDatabase = async (req, res, next) => {
-    if (req.oidc && req.oidc.user) {
-        const { email, sub } = req.oidc.user;
-        const [provider, provider_id] = sub.split('|');
+  if (req.oidc && req.oidc.user) {
+    const { email, user_id, nickname, picture } = req.oidc.user;
+    const [provider, provider_id] = user_id.split("|");
 
-        try {
-            // Check if user exists
-            const { rows: existingUser } = await client.execute({
-                sql:"SELECT * FROM users WHERE provider = ? AND provider_id = ?",
-                args: [provider, provider_id]
+    try {
+      const { rows: existingUser } = await client.execute({
+        sql: "SELECT * FROM users WHERE provider = ? AND provider_id = ?",
+        args: [provider, provider_id],
+      });
 
-            });
-
-            if (!existingUser.length) {
-                // Insert new user
-                await client.execute({
-                    sql: "INSERT INTO users (email, provider, provider_id) VALUES (?,?,?)",
-                    args: [email, provider, provider_id]
-                });
-                console.log("New OAuth user inserted into database");
-            } else {
-                console.log("OAuth user already exists in the database");
-            }
-        } catch (error) {
-            console.error("Error checking or inserting user into the database:", error);
-        }
+      if (!existingUser.length) {
+        await client.execute({
+          sql: "INSERT INTO users (email, provider, provider_id, username, profile_picture) VALUES (?,?,?,?,?)",
+          args: [email, provider, provider_id, nickname, picture],
+        });
+        console.log("New OAuth user inserted into database");
+      } else {
+        console.log("OAuth user already exists in the database");
+      }
+    } catch (error) {
+      console.error("Error checking or inserting user into the database:", error);
     }
-    next();
+  }
+  next();
 };
 
 export { authMiddleware, checkUserInDatabase };
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import { authMiddleware, checkUserInDatabase } from './middlewares/auth0.js';
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import { authMiddleware, checkUserInDatabase } from "./middlewares/auth0.js";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
-import userRoutes from './routes/userRoutes.js';
-import productRoutes from './routes/productRoutes.js';
-import cartRoutes from './routes/cartRoutes.js';
-import authRoutes from './routes/authRoutes.js';
+import userRoutes from "./routes/userRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cartRoutes from "./routes/cartRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 const port = 3000;
@@ -24,17 +24,17 @@ app.use(checkUserInDatabase);
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Use routes
-app.use('/api/users', userRoutes);
-app.use('/api/products', productRoutes);
-app.use('/api/cart', cartRoutes);
-app.use('/', authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/", authRoutes);
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
