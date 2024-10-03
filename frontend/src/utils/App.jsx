acf135eeb19971f2aee22a77fddbda21412ef0ed
Kosmo <luismg0408@gmail.com>
2024-10-03 21:04:48 +0200

    Frontend Folder Structure

--- frontend/src/utils/App.jsx
+++ frontend/src/utils/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from '../assets/images/react.svg'
+import viteLogo from '../assets/images/vite.svg'
 import './App.css'
 
 function App() {
