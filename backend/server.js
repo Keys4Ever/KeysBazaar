7ffdf77efe889a1d7482f52c06ce31eec1073ffa
Kosmo <luismg0408@gmail.com>
2024-10-11 20:43:55 +0200

    Feat: CORS implementation

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { authMiddleware, checkUserInDatabase } from "./middlewares/auth0.js";
 import dotenv from "dotenv";
 import path from "path";
@@ -16,10 +17,11 @@ const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Auth0 middleware for login
+// CORS (All routes, change later)
+app.use(cors());
+
 app.use(authMiddleware);
 
-// Middleware to check and insert the user in the database
 app.use(checkUserInDatabase);
 
 app.use(express.json());
@@ -28,13 +30,11 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// Use routes
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/", authRoutes);
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
