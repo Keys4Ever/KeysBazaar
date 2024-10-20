a76081f390f097c5bd3f6d7263c83bfac45eedee
Keys4Ever <cubillagonzalo@outlook.com>
2024-10-20 16:16:06 -0300

    Feat: Product page now uses the product from backend.

--- frontend/src/pages/Product/ProductPage.jsx
+++ frontend/src/pages/Product/ProductPage.jsx
@@ -1,22 +1,56 @@
 import { useParams } from 'react-router-dom';
-import mockupProduct from '@utils/mockup.json';
 import './ProductPage.css';
 import ProductSlider from '@components/ProductSlider/ProductSlider';
+import { useEffect, useState } from 'react';
 
 const ProductPage = () => {
     const { id } = useParams();
-    const product = mockupProduct.find((p) => p.productId === id);
+    const [product, setProduct] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            try {
+                const response = await fetch(`http://localhost:3000/api/products/${id}`);
+                if (!response.ok) {
+                    throw new Error(`Can't find product with id ${id}`);
+                }
+                const data = await response.json();
+                setProduct(data);
+            } catch (error) {
+                console.error(error);
+                alert(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchProduct();
+    }, [id]);
+
     function handleAddCartButton() {
-        //Kaypi chayanchis hukchasqayta karuta yapaypaq.
-        alert(`Has agregado el producto "${product.name}" al carrito`);
+        if (product) {
+            //Kaypi chayanchis hukchasqayta karuta yapaypaq.
+            alert(`Has agregado el producto "${product.title}" al carrito`);
+        }
     }
-    window.scrollTo(0,0);
+
+    window.scrollTo(0, 0);
+
+    if (loading) {
+        return <div>Cargando...</div>;
+    }
+
+    if (!product) {
+        return <div>No se encontró el producto.</div>;
+    }
+
     return (
         <>
             <div className='product-page' id='asd'>
-                <img className='product-image' src={product.banner} alt={product.name} />
+                <img className='product-image' src={product.imageUrl} alt={product.title} />
                 <div className='product-info'>
-                    <h1 className='product-title'>{product.name}</h1>
+                    <h1 className='product-title'>{product.title}</h1>
                     <p className='product-price'>Precio: ${product.price}</p>
                     <button className='product-button' onClick={handleAddCartButton}>Agregar al carrito</button>
                     <p className='product-details'>{product.description}</p>
@@ -24,7 +58,6 @@ const ProductPage = () => {
             </div>
             <hr/>
             <ProductSlider />
-
         </>
     );
 };
